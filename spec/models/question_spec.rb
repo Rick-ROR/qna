cba72a4e7afc9f2164569855821d64e19541bcc7
Rick-ROR <Rick.ROR@ya.ru>
2019-06-28 20:13:05 +0000

    rewrote rspec test validations with shoulda-matchers

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,16 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-  it 'validates presence of title' do
-    question = Question.new(body: '123')
-    # expect(question.valid?).to be_falsey
-    # варинт для предикатов в rspec:
-    expect(question).to_not be_valid
-  end
-
-  it 'validates presence of body' do
-    question = Question.new(title: '123')
-    expect(question).to_not be_valid
-  end
+  it{ should validate_presence_of :title}
+  it{ should validate_presence_of :body}
 end
