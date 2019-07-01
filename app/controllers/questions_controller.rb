43eb5e7e86a3ca15d01a8c03edcfb299cc087fd4
Rick-ROR <Rick.ROR@ya.ru>
2019-07-01 15:57:24 +0000

    added rspec-tests for controller Question#update for valid story

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
 
-  before_action :set_question, only: [:show, :edit]
+  before_action :set_question, only: [:show, :edit, :update]
   def index
     @questions = Question.all
   end
@@ -23,6 +23,11 @@ class QuestionsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    Question.update(question_params)
+    redirect_to @question
   end
 
   private
