fbe77e8eba15c41426c53f6537254059f5e5aae2
Rick-ROR <Rick.ROR@ya.ru>
2019-07-02 14:47:35 +0000

    added in controller Answers#show & rspecs for her

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,17 @@
 class AnswersController < ApplicationController
+
+  def show
+  end
+
+  private
+
+  def answer
+    @answer ||= params[:id] ? Answer.find(params[:id]) : Answer.new
+  end
+
+  helper_method :answer
+
+  # def answer_params
+  #   params.require(:answer).permit(:title, :body)
+  # end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
 
+  let(:question) { create(:question) }
+
+  describe 'GET #show' do
+    let(:answer) {create(:answer)}
+
+    # it 'assigns the requested answer to @answer' do
+    #   get :show, params: {id: answer}
+    #   expect(assigns(:answer)).to eq answer
+    # end
+
+    it 'renders show view' do
+      get :show, params: {id: answer}
+      expect(response).to render_template :show
+    end
+
+  end
+
 end
