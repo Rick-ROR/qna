8f1993d09b7ae9f946fb6712091343e455085383
Rick-ROR <Rick.ROR@ya.ru>
2019-07-06 20:23:19 +0000

    scenario 'Authenticated user asks a question'

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :questions do
+    post 'new', as: 'new', on: :collection
     resources :answers, shallow: true
   end
 
