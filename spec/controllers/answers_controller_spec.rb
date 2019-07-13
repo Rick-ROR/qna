58460d480ee69049d30fc695ab68a6d58f6bc5a7
Rick-ROR <Rick.ROR@ya.ru>
2019-07-13 18:04:56 +0000

    user-story: Only an authenticated user can create questions and answers.

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
-
   let(:question) { create(:question) }
-
+  let(:user) { create(:user) }
+  
   describe 'POST #create' do
+    before { login(user) }
+
     context 'with valid attributes' do
       let(:action) { post :create, params: { question_id: question, answer: attributes_for(:answer) } }
 
