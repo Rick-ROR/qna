d8e587f0e55af9a23402346d6951a616bcbab7d8
Rick-ROR <Rick.ROR@ya.ru>
2019-07-14 14:09:59 +0000

    added specs for author_of? in model User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,34 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+
+  describe 'author_of?' do
+    let(:author) { create(:user) }
+
+    context 'for the question' do
+      it 'returns true if the user is the author' do
+        question = create(:question, author: author)
+        expect(author).to be_an_author_of(question)
+      end
+
+      it 'returns false if the user is not the author' do
+        question = create(:question)
+        expect(author).not_to be_an_author_of(question)
+      end
+    end
+
+    context 'for the answer' do
+      it 'returns true if the user is the author' do
+        answer = create(:answer, author: author)
+        expect(author).to be_an_author_of(answer)
+      end
+
+      it 'returns false if the user is not the author' do
+        answer = create(:answer)
+        expect(author).not_to be_an_author_of(answer)
+      end
+    end
+  end
+
 end
