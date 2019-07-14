6e8587b62b829d0cdadddb67d4d0a4fa5e503e82
Rick-ROR <Rick.ROR@ya.ru>
2019-07-14 00:23:15 +0000

    implemented validate :correct in Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,5 +3,6 @@ class Answer < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
   # validates :body, :correct, presence: true
+  validates :correct, inclusion: { in: [true, false] }
   validates :body, presence: true
 end
