4c66f257cce695a4902a0e4efce6ba18f366125b
Rick-ROR <Rick.ROR@ya.ru>
2019-08-06 23:50:27 +0000

    implemented adding a files when creating a answer

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Answer, type: :model do
 
   it{ should validate_presence_of :body }
 
+  it 'have many attached files' do
+    expect(Answer.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+  end
+
   describe 'testing scopes:' do
     let!(:answers) { create_list(:answer, 4, question: create(:question)) }
     let!(:answer) { answers.last }
