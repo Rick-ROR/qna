f1a0a3f8a5c54a4ac2d38e291f129fdfb635d9cd
Rick-ROR <Rick.ROR@ya.ru>
2019-08-14 00:09:55 +0000

    added validation URL attr for Link

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,8 @@
 class Link < ApplicationRecord
+  URL_FORMAT = /\A(http|https):\/\/|[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?\z/ix
+
   belongs_to :linkable, polymorphic: true
 
-  validates :name, :url, presence: true
+  validates :name, presence: true
+  validates :url, presence: true, format: { with: URL_FORMAT, message: 'Ooops! URL has an invalid format.' }
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Link, type: :model do
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+
+  describe 'validate url' do
+    let(:question) { create(:question) }
+    let!(:valid) { build(:link, url: 'https://www.w3schools.com/cssref/css_selectors.asp', linkable: question) }
+    let!(:invalid) { build(:link, url: 'cssref/css_selectors', linkable: question) }
+
+    it { expect(valid).to be_valid }
+    it { expect(invalid).to be_invalid }
+
+    it do
+      invalid.validate
+      expect(invalid.errors[:url]).to include('Ooops! URL has an invalid format.')
+    end
+  end
 end
