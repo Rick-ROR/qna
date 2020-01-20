ec73e747893aecc17fadd0131f1425709579d155
Rick-ROR <Rick.ROR@ya.ru>
2020-01-20 21:40:44 +0000

    implemented User.find_for_oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,24 @@ class User < ApplicationRecord
   has_many :author_answers, foreign_key: 'author_id', class_name: 'Answer'
   has_many :author_votes, foreign_key: 'author_id', class_name: 'Vote'
   has_many :rewards, through: :author_answers
+  has_many :authorizations
 
   def author_of?(resource)
     id == resource.author_id
   end
 
   def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+    email = auth.info[:email]
+    user = User.where(email: email).first
 
+    unless user
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+    end
+
+    user.authorizations.create(provider: auth.provider, uid: auth.uid.to_s)
+    user
   end
 end
