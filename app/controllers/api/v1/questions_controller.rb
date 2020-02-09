a1d6a8c48e98c91290b84cf59f416e74cfc9c779
Rick-ROR <Rick.ROR@ya.ru>
2020-02-09 21:52:51 +0000

    implemented full api for questions

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,7 +1,47 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
+  authorize_resource class: Question
+
+  expose :questions, ->{ Question.all.order(created_at: :desc) }
+  expose :question, -> { params[:id] ? Question.with_attached_files.find(params[:id]) : Question.new }
 
   def index
-    @questions = Question.all
-    render json: @questions
+    render json: questions, each_serializer: QuestionsSerializer
+  end
+
+  def show
+    render json: question
+  end
+
+  def create
+    @question = current_resource_owner.author_questions.new(question_params)
+    if @question.save
+      render json: @question
+    else
+      render json: { errors: @question.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    authorize! :update, question
+    if question.update(question_params)
+      render json: question
+    else
+      render json: { errors: question.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    authorize! :destroy, question
+    question.destroy
+    render json: {}, status: :ok
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title,
+                                     :body,
+                                     links_attributes: [:name, :url, :id, :_destroy]
+                                     )
   end
 end
--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,9 +1,20 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :created_at, :updated_at, :short_title
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :title, :body, :created_at, :updated_at, :author_id, :short_title
+  has_many :links
+  has_many :files
+  has_many :comments
   has_many :answers
-  belongs_to :author
+
 
   def short_title
     object.title.truncate(7)
+  end
+
+  def files
+    object.files.map do |file|
+      rails_blob_path(file, only_path: true)
+    end
   end
 end
