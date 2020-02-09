830e1b9ecf215e3a773cb587772656c46fa48d06
Rick-ROR <Rick.ROR@ya.ru>
2020-02-09 00:43:43 +0000

    added /api/v1/profiles

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -26,13 +26,58 @@ describe 'Profiles API', type: :request do
         attrs = %w[id email admin created_at updated_at]
 
         attrs.each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json
+          expect(json['user'][attr]).to eq me.send(attr).as_json
         end
       end
 
       it 'does not return private fields' do
         %w[password encrypted_password].each do |attr|
-          expect(json).to_not have_key(attr)
+          expect(json['user']).to_not have_key(attr)
+        end
+      end
+    end
+
+  end
+
+  describe 'GET /api/v1/profiles' do
+
+    let(:api_path) {  '/api/v1/profiles' }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      let!(:users) { create_list(:user, 3) }
+      let(:me) { create(:user) }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+      let(:user_response) { json['users'].last }
+
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it 'returns 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'returns list of users' do
+        expect(json['users'].size).to eq 3
+      end
+
+      it 'returns list doesn\'t include me' do
+        expect(json['users']).to_not be_include(me)
+      end
+
+      it 'returns all public fields' do
+        attrs = %w[id email admin created_at updated_at]
+
+        attrs.each do |attr|
+          expect(user_response[attr]).to eq users.last.send(attr).as_json
+        end
+      end
+
+      it 'does not return private fields' do
+        %w[password encrypted_password].each do |attr|
+          expect(user_response).to_not have_key(attr)
         end
       end
     end
