7b0b3f740843218d0dd9b87a4fc7bf2e0b92864c
Rick-ROR <Rick.ROR@ya.ru>
2020-02-16 20:37:31 +0000

    fixs on review: part IV

--- app/services/daily_digest.rb
+++ app/services/daily_digest.rb
@@ -1,5 +1,9 @@
 class Services::DailyDigest
   def send_digest
+    questions = Question.where(created_at: Date.yesterday.all_day)
+
+    return if questions.empty?
+
     User.find_each do |user|
       DailyDigestMailer.digest(user).deliver_later
     end
