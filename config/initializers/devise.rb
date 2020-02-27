7df86a4c73f2da3253cb0a66349430abad9a8357
Rick-ROR <Rick.ROR@ya.ru>
2020-02-27 19:04:04 +0000

    some fixs

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,9 +18,13 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = Rails.application.credentials[Rails.env.to_sym][:mailer][:user]
+  if Rails.env.production?
+    config.mailer_sender = Rails.application.credentials[Rails.env.to_sym][:mailer][:user]
+  else
+    config.mailer_sender = 'qna@example.edu'
+  end
 
-  # Configure the class responsible to send e-mails.
+      # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
