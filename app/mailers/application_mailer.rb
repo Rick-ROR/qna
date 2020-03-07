78b8768f019fd4aeaa1b5fddb8d6ea9e4efc9899
Rick-ROR <Rick.ROR@ya.ru>
2020-03-07 02:34:20 +0000

    set email sender for env production

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  if Rails.env.production?
+    default from: Rails.application.credentials[Rails.env.to_sym][:mailer][:user]
+  else
+    default from: 'qna_dev@example.com'
+  end
+
   layout 'mailer'
 end
